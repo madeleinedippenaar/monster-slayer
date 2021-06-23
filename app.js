ae71ee9914e8b47ca3eefec4ff4262c1003c7f4e
madeleinedippenaar <madeleinedippenaar0@gmail.com>
2021-06-22 21:28:54 -0400

    added method for attack button

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 const app = Vue.createApp({
-    data() {
-        return {
-            playerHealth: 100,
-            monsterHealth: 100,
-            winner: null,
-            currentRound: 0,
-            logMessages: []
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+      winner: null,
+      currentRound: 0,
+      logMessages: [],
+    };
+  },
+  computed: {
+    monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      } else {
+          return { width: this.monsterHealth + "%" };
+      }
+    },
+    playerBarStyles() {
+        if(this.playerHealth < 0) {
+            return {width: "0%"};
+        } else {
+            return { width: this.playerHealth + "%" };
         }
-    },
-    computed: {
-        
-    },
-    watch: {
-
-    },
-    methods: {
-
     }
+  },
+  watch: {},
+  methods: {
+    attackMonster() {
+      const attackValue = getRandomValue(5, 8);
+      console.log(attackValue);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(8, 12);
+      this.playerHealth -= attackValue;
+    },
+  },
 });
 
+app.mount('#game');
